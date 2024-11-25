7a0d6a87aec1686cca398a0470c2f4e8d46015f9
henriqueacf <henrique.assis145@gmail.com>
2024-11-25 16:31:37 -0300

    feat: começando a colocar peças no tabuleiro

---
+++ xadrez-console/xadrex/Rei.cs
@@ -0,0 +1,17 @@
+﻿using xadrez_console.tabuleiro;
+
+namespace xadrez_console.xadrex
+{
+    public class Rei : Peca
+    {
+        public Rei(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return "R";
+        }
+    }
+}
