804de196b8671fffae26ed79231e3d9e10912509
henriqueacf <henrique.assis145@gmail.com>
2024-11-25 14:23:05 -0300

    feat: criando classe de posição

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using xadrez_console.tabuleiro;
 
 namespace xadrez_console {
     class Program {
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
+
+            Posicao P;
+            P = new Posicao(3, 4);
         }
     }
 }
