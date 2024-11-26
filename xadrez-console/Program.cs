a73e499c1ecc89dc1a06fb364ad56ef044b450ce
henriqueacf <henrique.assis145@gmail.com>
2024-11-26 14:19:10 -0300

    feat: criando classe posição xadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,14 +8,18 @@ namespace xadrez_console {
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                // Tabuleiro tab = new Tabuleiro(8, 8);
+                //
+                // tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                // tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                //
+                // tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                //
+                // Tela.imprimirTabuleiro(tab);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+                Console.WriteLine(pos);
 
-                Tela.imprimirTabuleiro(tab);
             }
             catch (TabuleiroException e)
             {
