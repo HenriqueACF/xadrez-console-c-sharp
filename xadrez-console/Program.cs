7450ce69383db5f9976d8748fa9144f87fb51fd1
henriqueacf <henrique.assis145@gmail.com>
2024-11-28 15:30:14 -0300

    feat: teste movimento das peças

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -18,6 +18,13 @@ namespace xadrez_console {
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.LerPosicaoXadrez().toPosicao();
+                    
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                    
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.LerPosicaoXadrez().toPosicao();
                     
